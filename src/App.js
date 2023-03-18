163fba84ebdc853c831d0582ca97144be6de8484
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-03-18 18:30:57 +0100

    made changes to meta description

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home";
 import Products from "./pages/Product/Products";
 import Contact from "./pages/Contact/Contact";
+import Discount from "./pages/Discount/Discount";
 import Gucci from "./pages/Products/Gucci";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}/> 
         <Route path="/products" element={<Products/>}/> 
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/discount" element={<Discount/>}/>
         <Route path="/gucci" element={<Gucci/>} />
       </Routes>
     </Router>
