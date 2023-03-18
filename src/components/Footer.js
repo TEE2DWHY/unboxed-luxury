393a896c7bc05803525a097c68e58164ce6a2d20
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-03-18 21:44:16 +0100

    completed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,36 @@
 import React from 'react'
-
+import logo from "../assets/images/logo.png"
 const Footer = () => {
   return (
         <>
             <div className='container footer d-flex justify-content-between'>
-                  <div className='brand-details'>
-                      <h3>Logo</h3>
-                      <p>Unboxed Luxury is a footwear brand that sells branded shoes that have being opened from their box.
+                  <div className='brand-details container'>
+                      <img className='img-fluid' src={logo} alt="logo"/>
+                      <br/> <br/>
+                      <p className='footer-p'>Unboxed Luxury is a footwear brand that sells branded shoes that have being opened from their box. Unboxed luxury is a footwear brand that sells branded shoes that have being opened from their box.
                       </p>
+                      <i class="fa-brands fa-square-whatsapp fa-2x" style={{padding:"20px"}}></i>
+                      <i class="fa-brands fa-twitter fa-2x" style={{padding:"20px"}}></i>
+                      <i class="fa-brands fa-square-instagram fa-2x" style={{padding:"20px"}}></i>
+                      <i class="fa-brands fa-tiktok fa-2x" style={{padding:"20px"}}></i>
+                      {/* <i class="fa-brands fa-facebook fa-2x"></i> */}
                   </div>
-                  <div className='about'>
-                      <h3>About</h3>
-                      <p>Our Company</p>
-                      <p>Customer Service</p>
-                      <p>Discount and Promos</p>
-                      <p>Terms and Policy</p>
-                  </div>
-                  <div className='product'>
-                      <h3>Product</h3>
-                      <p>Slides</p>
-                      <p>Sandals</p>
-                      <p>Shoes</p>
-                      <p>Sneakers</p>
-                  </div>
+                  <div className='footer-content container'>
+                    <div className='about'>
+                        <h3>About</h3>
+                        <p>Our Company</p>
+                        <p>Customer Service</p>
+                        <p>Discount and Promos</p>
+                        <p>Terms and Policy</p>
+                    </div>
+                    <div className='product'>
+                            <h3>Product</h3>
+                            <p>Slides</p>
+                            <p>Sandals</p>
+                            <p>Shoes</p>
+                            <p>Sneakers</p>
+                        </div>
+                  </div>    
             </div>
         </>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="/#"><img className='img-fluid logo' src='./images/logo.png' alt='logo'/></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container">
+                <a className="navbar-brand" href="/#"><img className='img-fluid logo' src='./images/logo.png' alt='logo'/></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active nav-home" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <Link className="nav-link active nav-home" aria-current="page" to="/">Home</Link>
                     </li>
-                    <li class="nav-item">
-                    <Link class="nav-link" to="/products">Product</Link>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/products">Product</Link>
                     </li>
-                    <li class="nav-item">
-                    <Link class="nav-link" to="/contact">Contact Us</Link>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/contact">Contact Us</Link>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/#">Discount</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/#">Discount</a>
                     </li>
                 </ul>
                 <span class="navbar-text nav-cart">
